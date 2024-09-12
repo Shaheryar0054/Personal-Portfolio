2fa4370cff7ad1af1f30ec36f8348448897998b1
shaheryar abid <sharyar0310@gmail.com>
2024-09-12 20:33:42 +0500

    Added Routing on the navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import { Navbar } from './components/Navbar';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
+
+  const router = createBrowserRouter ([
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path: "/About",
+      element: <About/>
+    },
+  ])
   return (
     <div className="container">
       <Navbar/>
+      <RouterProvider router= {router} />
     </div>
   );
 }
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import logo from '../assets/logo.png';
-
 export const Navbar = () => {
   return (
     <div>
         <header className='navbar'>
         <img src={logo} className="App-logo" alt="logo" />
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Tech Stack</li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <a href='/'><li>Home</li></a>
+          <a href='/About'><li>About</li></a>
+          <a href='/'><li>Tech Stack</li></a>
+          <a href='/'><li>Projects</li></a>
+          <a href='/'><li>Contact</li></a>
         </ul>
         </header>
     </div>
