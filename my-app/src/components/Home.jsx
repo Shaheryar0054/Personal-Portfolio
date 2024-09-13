4a81d493cf5336e2298340413167a60835240c77
shaheryar abid <sharyar0310@gmail.com>
2024-09-13 21:32:33 +0500

    Added Home component

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        This is my Home components
+        <h1>Hi 👋,<br/> My name is<br/>Shaheryar Abid<br/> I build things for web</h1>
     </div>
   )
 }
